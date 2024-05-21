d45103113fabacda11bf5606cfce773d930dae50
victorppeixoto <victorpinange@outlook.com>
2024-05-21 09:55:40 -0300

    ok

--- CursoCSharp/Encapsulamento/obj/Debug/netcoreapp2.0/Encapsulamento.AssemblyInfo.cs
+++ CursoCSharp/Encapsulamento/obj/Debug/netcoreapp2.0/Encapsulamento.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Encapsulamento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+464fc7fcce26afcf46da1295e099c9087e1b9c2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Encapsulamento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Encapsulamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
